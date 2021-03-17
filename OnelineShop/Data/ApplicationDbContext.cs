21cde701e2a8bdc5b32f5e62bc8a6133ec9db108
Rokibul21 <50902417+Rokibul21@users.noreply.github.com>
2021-03-17 23:21:59 +0600

    ApplicationModeladdedinDB

--- OnelineShop/Data/ApplicationDbContext.cs
+++ OnelineShop/Data/ApplicationDbContext.cs
@@ -18,5 +18,6 @@ namespace OnelineShop.Data
         public DbSet<Products> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
---
+++ OnelineShop/Migrations/20210317171813_ApplicationModelAdd.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnelineShop.Migrations
+{
+    public partial class ApplicationModelAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_orders_OrderId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_orders",
+                table: "orders");
+
+            migrationBuilder.RenameTable(
+                name: "orders",
+                newName: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FristName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_Orders_OrderId",
+                table: "OrderDetails",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderDetails_Orders_OrderId",
+                table: "OrderDetails");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Orders",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FristName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameTable(
+                name: "Orders",
+                newName: "orders");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_orders",
+                table: "orders",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderDetails_orders_OrderId",
+                table: "OrderDetails",
+                column: "OrderId",
+                principalTable: "orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
