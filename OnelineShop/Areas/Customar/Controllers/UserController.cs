43c35fb5735508c195e48a541fab48f9a1074f9f
Rokibul21 <50902417+Rokibul21@users.noreply.github.com>
2021-03-19 12:39:58 +0600

    UserController Add

---
+++ OnelineShop/Areas/Customar/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OnelineShop.Models;
+
+namespace OnelineShop.Areas.Customar.Controllers
+{
+    [Area("Customar")]
+    public class UserController : Controller
+    {
+        UserManager<IdentityUser> _userManager;
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ApplicationUser user)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _userManager.CreateAsync(user, user.PasswordHash);
+                if (result.Succeeded)
+                {
+                    TempData["save"] = "User Has Been Save Successfuly";
+                    return RedirectToAction(nameof(Index));
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View();
+        }
+    }
+}
--- OnelineShop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ OnelineShop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using OnelineShop.Models;
 
 namespace OnelineShop.Areas.Identity.Pages.Account
 {
@@ -66,7 +67,7 @@ namespace OnelineShop.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new  ApplicationUser{ UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
